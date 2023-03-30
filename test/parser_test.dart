836a3336d56be156dfe539b67c0be0fd63fdc494
ChessMax <chessmax@yandex.ru>
2023-03-30 20:36:23 +0300

    Fix tests.

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -7,14 +7,15 @@ import 'package:test/scaffolding.dart';
 void main() {
   test('Should parse expressions', () {
     final inputs = <String, String>{
-      '(2)': '2.0',
-      '-5': '-5.0',
-      '!true': '!true',
-      '!!!!(true)': '!!!!true',
-      '8 - 3 * 2': '(8.0 - (3.0 * 2.0))',
-      '(8 - 3) * 2': '((8.0 - 3.0) * 2.0)',
-      '5 + 4 > 4': '((5.0 + 4.0) > 4.0)',
-      '8 * 2 == 4 + 4 + 4 + 4': '((8.0 * 2.0) == (((4.0 + 4.0) + 4.0) + 4.0))',
+      '(2);': '2.0;',
+      '-5;': '-5.0;',
+      '!true;': '!true;',
+      '!!!!(true);': '!!!!true;',
+      '8 - 3 * 2;': '(8.0 - (3.0 * 2.0));',
+      '(8 - 3) * 2;': '((8.0 - 3.0) * 2.0);',
+      '5 + 4 > 4;': '((5.0 + 4.0) > 4.0);',
+      '8 * 2 == 4 + 4 + 4 + 4;':
+          '((8.0 * 2.0) == (((4.0 + 4.0) + 4.0) + 4.0));',
       // 'true and false': '(true and false)',
       // 'false or false': '(false or false)',
       // 'false or true and false': '(false or (true and false))',
