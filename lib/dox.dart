09ae643f7d52d1cfd4895f95a8fd69d58deb92d3
ChessMax <chessmax@yandex.ru>
2023-03-30 17:56:45 +0300

    Simple Repl and run script functionality added.

--- lib/dox.dart
+++ lib/dox.dart
@@ -1,10 +1,21 @@
+import 'dart:io';
+
+import 'package:dox/interpreter.dart';
 import 'package:dox/lexer.dart';
 import 'package:dox/parser.dart';
-import 'package:dox/visitor.dart';
 
 abstract class Dox {
   static bool _hadError = false;
   static bool _hadRuntimeError = false;
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  static void report(int line, String where, String message) {
+    print('[line $line] Error$where: $message');
+    _hadError = true;
+  }
 
   static void runtimeError(Object error) {
     print(error);
@@ -12,23 +23,59 @@ abstract class Dox {
   }
 
   static Future<void> main(List<String> args) async {
-    parseAndPrint('(2)');
-    parseAndPrint('-5');
-    parseAndPrint('!true');
-    parseAndPrint('!!!!(true)');
-    parseAndPrint('8 - 3 * 2');
-    parseAndPrint('(8 - 3) * 2');
+    if (args.length > 1) {
+      print('Usage: dox [script]');
+      exit(64);
+    } else if (args.length == 1) {
+      final program = await File(args[0]).readAsString();
+      run(program);
+    } else {
+      runRepl();
+    }
   }
 
-  static void parseAndPrint(String program) {
+  static void run(String program) {
     try {
+      final interpreter = Interpreter();
       final tokens = Lexer.enumerate(program);
       final parser = Parser(tokens: tokens.toList());
       final expr = parser.parse();
-      final printer = PrintVisitor();
-      print(expr.accept(printer));
+
+      if (_hadError) exit(65);
+
+      interpreter.evaluate(expr);
     } catch (e) {
       print(e);
+      exit(65);
+    }
+  }
+
+  static void runRepl() {
+    final interpreter = Interpreter();
+    while (true) {
+      try {
+        stdout.write('> ');
+        final input = stdin.readLineSync();
+        if (input == null) break;
+        final tokens = Lexer.enumerate(input);
+        final parser = Parser(tokens: tokens.toList());
+        final expr = parser.parse();
+        if (_hadError) {
+          _hadError = false;
+          print('Parsing error: ');
+          continue;
+        }
+
+        interpreter.evaluate(expr);
+
+        if (_hadRuntimeError) {
+          _hadRuntimeError = false;
+          print('Runtime error: ');
+          continue;
+        }
+      } catch (e) {
+        print('Unexpected error: $e');
+      }
     }
   }
 }
